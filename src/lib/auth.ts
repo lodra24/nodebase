9f1b029fd6edc044d3c801a53d41b3a79cf3f92a
lodra24 <orhanbulbul@gmail.com>
2025-10-30 16:53:16 +0300

    feat(billing): integrate Polar for subscription management

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { db } from "./db";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(db, {
@@ -10,4 +12,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "c23d6e66-ae56-418e-b840-7493359f8099",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
